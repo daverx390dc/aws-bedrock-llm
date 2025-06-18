ae9278d44ff1c8dccb3b7f34e4edefdf47350e7c
daverx390dc <davechinmayb9@gmail.com>
2025-06-18 12:53:35 +0530

    update file

--- qa_chain.py
+++ qa_chain.py
@@ -8,9 +8,19 @@ from utils import load_raw_documents, chunk_raw_documents
 def get_qa_chain():
     embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1")
     vectorstore = FAISS.load_local(VECTOR_DB_PATH, embeddings=embeddings, allow_dangerous_deserialization=True)
-    retriever = vectorstore.as_retriever(search_kwargs={'k': 3})
+    retriever = vectorstore.as_retriever()
 
-    llm = BedrockLLM(model_id="meta.llama3-70b-instruct-v1:0")
+    prompt = "Read the document and answer the quesion asked."
+
+
+    llm = BedrockLLM(model_id="meta.llama3-70b-instruct-v1:0",
+                     model_kwargs = {
+        "prompt": prompt,
+        "temperature": 0.3,  # ✅ spelled correctly
+        "top_p": 0.95,
+        "max_gen_len": 512
+        }
+                                                 )
 
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
